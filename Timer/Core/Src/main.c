14ede454066522d7ce6a7b6de47574a0bdca8fab
DESKTOP-7Z\Genki <Genki4000@gmail.com>
2024-09-19 13:56:21 +0700

    Init for Ex2

--- Timer/Core/Src/main.c
+++ Timer/Core/Src/main.c
@@ -94,10 +94,30 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+	setTimer1(50);
+    setTimer2(50);
+    HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,  GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(EN1_GPIO_Port, EN2_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(Dot_GPIO_Port, Dot_Pin, GPIO_PIN_SET);
+    Display7Seg(1);
   while (1)
   {
     /* USER CODE END WHILE */
+	  if(timer1_flag == 1){
+	  		  setTimer1(50);
+	  		  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_RESET);
+	  		  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,  GPIO_PIN_SET);
+	  		  Display7Seg(2);
 
+	  	  }
+	  	  if(timer2_flag == 1){
+	  		    setTimer2(50);
+	  		    HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+	  		    HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,  GPIO_PIN_RESET);
+	  	  		Display7Seg(1);
+	  	  }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -197,18 +217,21 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, Dot_Pin|LED_RED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, LEDA_Pin|LEDB_Pin|LEDC_Pin|LEDD_Pin
                           |LEDE_Pin|LEDF_Pin|LEDG_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : LED_RED_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin;
+  /*Configure GPIO pins : Dot_Pin LED_RED_Pin EN0_Pin EN1_Pin
+                           EN2_Pin EN3_Pin */
+  GPIO_InitStruct.Pin = Dot_Pin|LED_RED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_RED_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LEDA_Pin LEDB_Pin LEDC_Pin LEDD_Pin
                            LEDE_Pin LEDF_Pin LEDG_Pin */
@@ -223,7 +246,9 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-	timerRun();
+	if (htim->Instance == TIM2) {
+	        timerRun();
+	    }
 }
 /* USER CODE END 4 */
 
